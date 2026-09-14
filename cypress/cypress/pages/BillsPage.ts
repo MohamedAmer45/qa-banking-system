01484cb56fe69a1c5372c59a7776f7e8744e2e19
Mohamedamer45 <mohamed.amer45312@gmail.com>
2026-09-14 23:11:46 +0300

    Add Cypress bills module

---
+++ cypress/cypress/pages/BillsPage.ts
@@ -0,0 +1,110 @@
+class BillsPage {
+
+  private readonly customerLogin =
+    "#login button.enter[data-role='customer']";
+
+  private readonly billsNav =
+    "#nav button[data-s='bills']";
+
+  private readonly billsSection =
+    "#bills";
+
+  private readonly billerSelect =
+    "#biller";
+
+  private readonly amountInput =
+    "#billAmount";
+
+  private readonly submitButton =
+    "#billForm button";
+
+  private readonly toast =
+    "#toast";
+
+
+  visitAsCustomer(): void {
+
+    cy.visit("/");
+
+    cy.get(this.customerLogin)
+      .should("be.visible")
+      .and("be.enabled")
+      .click();
+
+  }
+
+
+  openBills(): void {
+
+    cy.get(this.billsNav)
+      .should("be.visible")
+      .and("be.enabled")
+      .click();
+
+  }
+
+
+  getBillsSection() {
+    return cy.get(this.billsSection);
+  }
+
+
+  getBillerSelect() {
+    return cy.get(this.billerSelect);
+  }
+
+
+  getAmountInput() {
+    return cy.get(this.amountInput);
+  }
+
+
+  getSubmitButton() {
+    return cy.get(this.submitButton);
+  }
+
+
+  getToast() {
+    return cy.get(this.toast);
+  }
+
+
+  selectBiller(biller: string): void {
+
+    this.getBillerSelect()
+      .select(biller);
+
+  }
+
+
+  enterAmount(amount: string): void {
+
+    this.getAmountInput()
+      .clear()
+      .type(amount);
+
+  }
+
+
+  submit(): void {
+
+    this.getSubmitButton()
+      .click();
+
+  }
+
+
+  payBill(
+    biller: string,
+    amount: string
+  ): void {
+
+    this.selectBiller(biller);
+    this.enterAmount(amount);
+    this.submit();
+
+  }
+
+}
+
+export default new BillsPage();
